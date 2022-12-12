8344586ef3df5afbc7155fc3a6e48f72212c7c49
ashpoghosh <42296513+ashpoghosh@users.noreply.github.com>
2022-12-12 22:17:31 +0530

    Update spark_submit.sh

--- spark_submit.sh
+++ spark_submit.sh
@@ -1,14 +1,14 @@
 mkdir data
-unzip Data.zip -d data/
+unzip Data.zip -d
 
-mkdir output_files/analysis_1
-mkdir output_files/analysis_2
-mkdir output_files/analysis_3
-mkdir output_files/analysis_4
-mkdir output_files/analysis_5
-mkdir output_files/analysis_6
-mkdir output_files/analysis_7
-mkdir output_files/analysis_8
+mkdir -p output_files/analysis_1
+mkdir -p output_files/analysis_2
+mkdir -p output_files/analysis_3
+mkdir -p output_files/analysis_4
+mkdir -p output_files/analysis_5
+mkdir -p output_files/analysis_6
+mkdir -p output_files/analysis_7
+mkdir -p output_files/analysis_8
 
 spark-submit \
 --master "local[*]" \
