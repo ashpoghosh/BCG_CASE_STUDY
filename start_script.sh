1bf8e4026b91a3d4f9f13260b90753e51993af4f
ashpoghosh <42296513+ashpoghosh@users.noreply.github.com>
2022-12-13 09:11:32 +0530

    Update start_script.sh

--- start_script.sh
+++ start_script.sh
@@ -1,6 +1,4 @@
-mkdir data
 unzip Data.zip
-
 mkdir -p output_files/analysis_1
 mkdir -p output_files/analysis_2
 mkdir -p output_files/analysis_3
@@ -9,7 +7,6 @@ mkdir -p output_files/analysis_5
 mkdir -p output_files/analysis_6
 mkdir -p output_files/analysis_7
 mkdir -p output_files/analysis_8
-
 spark-submit \
 --master "local[*]" \
 --files configs/config.json \
